1f7bc4d9f42b27efd55e78afe0d9b25b3fa3a6d6
Howard Lau <howardlau1999@hotmail.com>
2023-11-12 00:06:27 +0800

    script: update windows bat script

--- compile.bat
+++ compile.bat
@@ -3,6 +3,6 @@ chcp 65001>nul
 set Path=%cd%\;%Path%
 set TYPST_FONT_PATHS=%cd%\fonts
 echo 编译中……
-typst --font-path fonts compile thesis.typ
+typst compile --font-path fonts thesis.typ
 echo 命令运行完毕，按任意键退出
 pause>nul
