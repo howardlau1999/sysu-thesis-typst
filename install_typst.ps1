32883578bac74074d4145892e714588879f1bf73
Howard Lau <howardlau1999@hotmail.com>
2024-04-29 19:54:53 +0800

    script: use tuna mirror for rustup

--- install_typst.ps1
+++ install_typst.ps1
@@ -1,6 +1,9 @@
 ﻿# Install Rustup with Powershell
 $OutputEncoding = [System.Text.Encoding]::UTF8
 
+# Use TUNA rustup mirror
+$Env:RUSTUP_DIST_SERVER = "https://mirrors.tuna.tsinghua.edu.cn/rustup"
+
 # Check if Rust is already installed
 if (Get-Command cargo -ErrorAction SilentlyContinue) {
   Write-Output "检测到 Rust 已安装，尝试更新。"
@@ -13,8 +16,7 @@ else {
 
   # Run the installer
   Write-Output "运行 Rustup 安装程序..."
-  $Env:RUSTUP_DIST_SERVER = "https://mirrors.tuna.tsinghua.edu.cn/rustup"
-  Start-Process —FilePath $Env:Temp/rustup-init.exe -Args -y
+  Start-Process -FilePath $Env:Temp/rustup-init.exe -Args -y
 
   # Add Rustup to the PATH
   Write-Output "将 Rustup 添加到 PATH 环境变量..."
